0abe0c93d61b5733af0fa0d015f5cf7901ef193c
DarkChill42 <borisbodvanski@gmail.com>
2026-04-21 11:50:19 +0200

    initial commit

---
+++ ProjectRealizationService/src/main/java/org/example/projectrealizationservice/ProjectRealizationServiceApplication.java
@@ -0,0 +1,24 @@
+package org.example.projectrealizationservice;
+
+import org.neo4j.cypherdsl.core.renderer.Configuration;
+import org.neo4j.cypherdsl.core.renderer.Dialect;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProjectRealizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRealizationServiceApplication.class, args);
+    }
+
+    @Bean
+    Configuration cypherDslConfiguration() {
+        return Configuration.newConfig()
+                .withDialect(Dialect.NEO4J_5).build();
+    }
+}
---
+++ ProjectRealizationService/src/test/java/org/example/projectrealizationservice/ProjectRealizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectrealizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRealizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
