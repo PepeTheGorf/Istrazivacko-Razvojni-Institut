6a5082006d638f650b750fb7e97a529c21f33ff7
DarkChill42 <borisbodvanski@gmail.com>
2026-04-28 12:00:07 +0200

    feat: implemented controller methods (CRUD endpoints and complex query endpoints)

--- ProjectRealizationService/src/main/java/org/example/projectrealizationservice/controller/TestController.java
+++ ProjectRealizationService/src/main/java/org/example/projectrealizationservice/controller/TestController.java
@@ -1,15 +1,25 @@
 package org.example.projectrealizationservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.projectrealizationservice.dto.ProjectTaskDTO;
+import org.example.projectrealizationservice.model.*;
+import org.example.projectrealizationservice.repository.ProjectRepository;
+import org.example.projectrealizationservice.repository.TaskRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
-
+    
     @GetMapping
     public String test() {
-        return "Hello from Project Realization Service!";
+        return "Test endpoint is working!";
     }
 }
