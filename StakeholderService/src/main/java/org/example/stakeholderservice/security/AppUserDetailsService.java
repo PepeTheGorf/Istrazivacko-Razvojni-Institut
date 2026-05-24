49d5676c9ff56bdea4733368faa60db08ccfee80
DarkChill42 <borisbodvanski@gmail.com>
2026-05-24 21:45:08 +0200

    feat: implemented JWT authentication in stakeholder service

---
+++ StakeholderService/src/main/java/org/example/stakeholderservice/security/AppUserDetailsService.java
@@ -0,0 +1,34 @@
+package org.example.stakeholderservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.example.stakeholderservice.model.User;
+import org.example.stakeholderservice.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(this::toUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
+
+    public UserDetails toUserDetails(User user) {
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())))
+                .build();
+    }
+}
